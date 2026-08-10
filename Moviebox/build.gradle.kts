c4de2329c05c8b4542b4817372b44c6e2fd16da5
noritoshikamou <noritoshi.kamou@gmail.com>
2026-08-10 15:52:58 +0700

    Update build.gradle.kts

--- Moviebox/build.gradle.kts
+++ Moviebox/build.gradle.kts
@@ -1,1 +1,15 @@
+version = 1
 
+cloudstream {
+    language = "id"
+    description = "Movibox - Movie subtitle indonesia"
+    authors = listOf("Asm0d3usX")
+	isCrossPlatform = true
+    status = 1
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie",
+        "Anime",
+        "AsianDrama",
+    )
+}
