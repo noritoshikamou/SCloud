d604e2a41251c8ff76878b6e614536d38cc25910
noritoshikamou <noritoshi.kamou@gmail.com>
2026-08-10 22:25:02 +0700

    Update LayarKaca21Plugin.kt

--- LayarKaca21/src/main/kotlin/com/LayarKaca21/LayarKaca21Plugin.kt
+++ LayarKaca21/src/main/kotlin/com/LayarKaca21/LayarKaca21Plugin.kt
@@ -1,1 +1,14 @@
+package com.LayarKaca21
 
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.baseprovider.extractor.ProviderExtractors
+
+@CloudstreamPlugin
+class LayarKaca21Plugin: BasePlugin() {
+    override fun load() {
+        val api = LayarKaca21()
+        registerMainAPI(api)
+        ProviderExtractors.filtered(api.config.allowedExtractors).forEach { registerExtractorAPI(it) }
+    }
+}
