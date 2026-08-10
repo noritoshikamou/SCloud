8e28a5265c3fe643d7ea9fb0b1ad652395d7e663
noritoshikamou <noritoshi.kamou@gmail.com>
2026-08-10 15:55:24 +0700

    Update MovieboxPlugin.kt

--- Moviebox/src/main/kotlin/com/Moviebox/MovieboxPlugin.kt
+++ Moviebox/src/main/kotlin/com/Moviebox/MovieboxPlugin.kt
@@ -1,1 +1,12 @@
+package com.moviebox
 
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class MovieboxPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(Moviebox())
+    }
+}
