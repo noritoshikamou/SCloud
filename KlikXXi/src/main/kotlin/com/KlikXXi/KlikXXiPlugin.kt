5aad3127e5e13f1702ea123c185c359905ec0ae9
noritoshikamou <noritoshi.kamou@gmail.com>
2026-08-10 21:18:45 +0700

    Update KlikXXiPlugin.kt

--- KlikXXi/src/main/kotlin/com/KlikXXi/KlikXXiPlugin.kt
+++ KlikXXi/src/main/kotlin/com/KlikXXi/KlikXXiPlugin.kt
@@ -1,1 +1,13 @@
+package com.klikxxi
 
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class KlikXXiPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(KlikXXi())
+		registerExtractorAPI(Hglink())
+    }
+}
