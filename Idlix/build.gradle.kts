034594b04ccc1a506c40d3385c7dc207d13ab2ac
noritoshikamou <noritoshi.kamou@gmail.com>
2026-08-10 15:47:21 +0700

    Update build.gradle.kts

--- Idlix/build.gradle.kts
+++ Idlix/build.gradle.kts
@@ -1,1 +1,15 @@
+version = 2
 
+cloudstream {
+    language = "id"
+    description = "Idlix - Movie subtitle indonesia"
+    authors = listOf("Hexated", "Phisher98", "Asm0d3usX")
+	isCrossPlatform = true
+    status = 1
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie",
+        "Anime",
+        "AsianDrama",
+    )
+}
