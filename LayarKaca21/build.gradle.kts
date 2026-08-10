ef59acd2fd5e0a3569b2cd4f18cc5acdcb7d32d9
noritoshikamou <noritoshi.kamou@gmail.com>
2026-08-10 22:23:01 +0700

    Update build.gradle.kts

--- LayarKaca21/build.gradle.kts
+++ LayarKaca21/build.gradle.kts
@@ -1,1 +1,21 @@
+// use an integer for version numbers
+version = 3
 
+cloudstream {
+    description = "🎬 LayarKaca21 - Nonton film box office, series, dan Asian drama dengan subtitle Indonesia gratis!"
+    language = "id"
+    authors = listOf("Imam2nd")
+
+    status = 1
+
+    tvTypes =
+        listOf(
+            "AsianDrama",
+            "TvSeries",
+            "Movie",
+        )
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=d21.team&sz=%size%"
+
+    isCrossPlatform = false
+}
