c238a1f012fcff2f0b2b7e38db1cc239403af02f
noritoshikamou <noritoshi.kamou@gmail.com>
2026-08-11 09:31:26 +0700

    Update AnichinPlugin.kt

--- Anichin/src/main/kotlin/com/Anichin/AnichinPlugin.kt
+++ Anichin/src/main/kotlin/com/Anichin/AnichinPlugin.kt
@@ -1,1 +1,14 @@
+package com.Anichin
 
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.baseprovider.extractor.ProviderExtractors
+
+@CloudstreamPlugin
+class AnichinPlugin: BasePlugin() {
+    override fun load() {
+        val api = Anichin()
+        registerMainAPI(api)
+        ProviderExtractors.filtered(api.config.allowedExtractors).forEach { registerExtractorAPI(it) }
+    }
+}
