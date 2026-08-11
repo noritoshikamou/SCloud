e519cc265a400d99aa336eebcc761de4f7936d00
noritoshikamou <noritoshi.kamou@gmail.com>
2026-08-11 09:29:31 +0700

    Update build.gradle.kts

--- Anichin/build.gradle.kts
+++ Anichin/build.gradle.kts
@@ -1,1 +1,16 @@
+// use an integer for version numbers
+version = 3
 
+cloudstream {
+    description = "🎬 Anichin - Streaming Anime China Subtitle Indonesia"
+    language = "id"
+    authors = listOf("NivinCNC")
+
+    status = 1
+
+    tvTypes = listOf("Anime")
+    iconUrl =
+        "https://i.ibb.co.com/vCTQt6Qn/Anichin.png"
+
+    isCrossPlatform = false
+}
